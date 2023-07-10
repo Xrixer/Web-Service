761311a87fde571b1b9b7e681c25ab0c7a0bbb0d
Xrixer <juanjogonzalez003@gmail.com>
2023-07-09 22:11:24 -0400

    Actualizacion Menu

--- Menu.py
+++ Menu.py
@@ -75,6 +75,124 @@ def list_menu():
         else:
             print("Opción inválida")
 
+def create_menu():
+    while True:
+        os.system('cls')
+        print("Menú de creación")
+        print("1. Persona")
+        print("2. Sección")
+        print("3. Escuela")
+        print("4. Facultad")
+        print("5. Volver")
 
+        choice = input("Selecciona una opción: ")
+
+        if choice == "1":
+            first_name = input("Ingresa el nombre: ")
+            last_name = input("Ingresa el apellido: ")
+            dni = input("Ingresa el DNI: ")
+            data = {'first_name': first_name, 'last_name': last_name, 'dni': dni}
+            response = requests.post('http://localhost:3000/persons', json=data)
+            if response.status_code == 201:
+                print("Persona creada exitosamente")
+            else:
+                print("Error al crear la persona")
+        elif choice == "2":
+            uc = input("Ingresa la UC: ")
+            semester = input("Ingresa el semestre: ")
+            type = input("Ingresa el tipo (mandatory o elective): ")
+            ht = input("Ingresa las horas teóricas: ")
+            hp = input("Ingresa las horas prácticas: ")
+            hl = input("Ingresa las horas de laboratorio: ")
+            data = {'uc': uc, 'semester': semester, 'type': type, 'ht': ht, 'hp': hp, 'hl': hl}
+            response = requests.post('http://localhost:3000/sections', json=data)
+            if response.status_code == 201:
+                print("Sección creada exitosamente")
+            else:
+                print("Error al crear la sección")
+        elif choice == "3":
+            name = input("Ingresa el nombre: ")
+            description = input("Ingresa la descripción: ")
+            data = {'name': name, 'description': description}
+            response = requests.post('http://localhost:3000/schools', json=data)
+            if response.status_code == 201:
+                print("Escuela creada exitosamente")
+            else:
+                print("Error al crear la escuela")
+        elif choice == "4":
+            name = input("Ingresa el nombre: ")
+            description = input("Ingresa la descripción: ")
+            data = {'name': name, 'description': description}
+            response = requests.post('http://localhost:3000/faculties', json=data)
+            if response.status_code == 201:
+                print("Facultad creada exitosamente")
+            else:
+                print("Error al crear la facultad")
+        elif choice == "5":
+            break
+        else:
+            print("Opción inválida")
+
+def update_menu():
+    while True:
+        os.system('cls')
+        print("Menú de actualización")
+        print("1. Actualizar persona")
+        print("2. Actualizar sección")
+        print("3. Actualizar escuela")
+        print("4. Actualizar facultad")
+        print("5. Volver")
+
+        choice = input("Selecciona una opción: ")
+
+        if choice == "1":
+            id = input("Ingresa el ID de la persona que deseas actualizar: ")
+            first_name = input("Ingresa el nuevo nombre: ")
+            last_name = input("Ingresa el nuevo apellido: ")
+            dni = input("Ingresa el nuevo DNI: ")
+            data = {'first_name': first_name, 'last_name': last_name, 'dni': dni}
+            response = requests.put(f'http://localhost:3000/persons/{id}', json=data)
+            if response.status_code == 200:
+                print("Persona actualizada exitosamente")
+            else:
+                print("Error al actualizar la persona")
+        elif choice == "2":
+            id = input("Ingresa el ID de la sección que deseas actualizar: ")
+            uc = input("Ingresa la nueva UC: ")
+            semester = input("Ingresa el nuevo semestre: ")
+            type = input("Ingresa el nuevo tipo (mandatory o elective): ")
+            ht = input("Ingresa las nuevas horas teóricas: ")
+            hp = input("Ingresa las nuevas horas prácticas: ")
+            hl = input("Ingresa las nuevas horas de laboratorio: ")
+            data = {'uc': uc, 'semester': semester, 'type': type, 'ht': ht, 'hp': hp, 'hl': hl}
+            response = requests.put(f'http://localhost:3000/sections/{id}', json=data)
+            if response.status_code == 200:
+                print("Sección actualizada exitosamente")
+            else:
+                print("Error al actualizar la sección")
+        elif choice == "3":
+            id = input("Ingresa el ID de la escuela que deseas actualizar: ")
+            name = input("Ingresa el nuevo nombre: ")
+            description = input("Ingresa la nueva descripción: ")
+            data = {'name': name, 'description': description}
+            response = requests.put(f'http://localhost:3000/schools/{id}', json=data)
+            if response.status_code == 200:
+                print("Escuela actualizada exitosamente")
+            else:
+                print("Error al actualizar la escuela")
+        elif choice == "4":
+            id = input("Ingresa el ID de la facultad que deseas actualizar: ")
+            name = input("Ingresa el nuevo nombre: ")
+            description = input("Ingresa la nueva descripción: ")
+            data = {'name': name, 'description': description}
+            response = requests.put(f'http://localhost:3000/faculties/{id}', json=data)
+            if response.status_code == 200:
+                print("Facultad actualizada exitosamente")
+            else:
+                print("Error al actualizar la facultad")
+        elif choice == "5":
+            break
+        else:
+            print("Opción inválida")
 
     
